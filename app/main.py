f1d4c11ecffae0fd509f1228b81e270008453546
Alejandro Quiroz <jquirozagudelo@gmail.com>
2026-02-18 01:42:04 -0500

    modificacion del main

--- app/main.py
+++ app/main.py
@@ -1,28 +1,7 @@
 from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
-from fastapi.requests import Request
 
-from app.procesos.inventario import router as inventario_router
-from app.procesos.agendamiento_v2 import router as agendamiento_v2_router
-from app.procesos.liquidacion import router as liquidacion_router
-
-
-# 🔹 Crear app
-app = FastAPI(title="Portal de Procesos Mexicargo")
-
-# 🔹 Templates y static
-templates = Jinja2Templates(directory="app/templates")
-app.mount("/static", StaticFiles(directory="app/static"), name="static")
-
-# 🔹 Routers
-app.include_router(inventario_router)
-app.include_router(agendamiento_v2_router)
-app.include_router(liquidacion_router)
+app = FastAPI()
 
 @app.get("/")
-async def home(request: Request):
-    return templates.TemplateResponse(
-        "index.html",
-        {"request": request}
-    )
+def test():
+    return {"status": "ok"}
